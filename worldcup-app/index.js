e375740534fcb308c56675d2a2cc900a68eeb4f0
WILDWEEGEE <ggross343@gmail.com>
2018-04-21 01:17:30 -0400

    Added simulation SQL queries to index.js

--- worldcup-app/index.js
+++ worldcup-app/index.js
@@ -27,13 +27,17 @@ app.get('/countryData', function(request, response) {
 
 app.use(express.static(__dirname + '/static/'));
 
+// ----------------------------------------------------------------------------
+// Simulation
 app.get('/getSimulation', function(request, response) {
-	connection.query('select * from country;',
+    var sql = getQuery(request.query.by);
+	connection.query(sql,
 	function (error, results, fields) {
 	  if (error) throw error;
 	  response.json(results);
 	});
 })
+// ----------------------------------------------------------------------------
 
 app.get('/getCountryData', function(request, response) {
 	connection.query('select * from country;',
@@ -43,32 +47,42 @@ app.get('/getCountryData', function(request, response) {
 	});
 })
 
-
-/*app.get('/friends', function(request, response) {
-	var mysql = require('mysql');
-	var connection = mysql.createConnection({
- 		host: 'fling.seas.upenn.edu',
-		user: 'jpopelka',
-		password: 'P3j5m4p9!',
-		database: 'jpopelka'
-	});
- 
-	connection.connect(function(err) {
-		if (err) {
-    		console.error('error connecting: ' + err.stack);
-    		return;
-  		}
- 
-	});
-	connection.query('SELECT p.name, count(p.name) FROM Friends f, Person p WHERE f.login=p.login GROUP BY p.name;',function(error, results, fields) {
-		if(error) {
-			console.error('error:' + error.stack);
-			return;
-		}
-		response.json(results);
-	});
-})*/
-
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+// ----------------------------------------------------------------------------
+// Simulation Functions
+
+// Get SQL Query
+function getQuery(by) {
+	var sql = 'SELECT * FROM country';
+	
+	if (by == "bElo" || by == "nElo") {
+		sql = 'SELECT country, team_group, elo_rating\
+			   FROM country;';
+	} else if (by == "star") {
+		sql = 'SELECT a.nationality as country, b.team_group as team_group, max(a.overall) as score\
+			   FROM player a JOIN country b on a.nationality=b.country\
+			   GROUP BY a.nationality, b.team_group;';
+	} else if (by == "wins") {
+		sql = 'SELECT country, team_group, count(*)-1 AS wins\
+			   FROM (SELECT country, team_group\
+					 FROM country\
+					 UNION ALL\
+					 SELECT a.winner AS country, b.team_group AS team_group\
+					 FROM world_cup_outcomes a JOIN country b ON a.winner=b.country) AS temp\
+			   GROUP BY country, team_group;';
+	} else if (by == "gdp") {
+		sql = 'SELECT country, team_group, gdp\
+			   FROM country;'
+	} else if (by == "capita") {
+		sql = 'SELECT country, team_group, gdp_per_capita\
+		       FROM country;'
+	} else {
+		console.error("Invalid criterion");
+		throw error;
+	}
+
+	return sql;
+}
